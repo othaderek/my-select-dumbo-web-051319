15623249915f7a1e71510aa6caf23740379917ed
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 21:04:17 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  new_arr = []
+  while i < collection.length
+    if yield(collection[i]) == true
+      new_arr << collection[i]
+    end
+    i += 1 
+  end
+  new_arr
 end
+
+nums = [1, 2, 3, 4, 5]
+
+my_select(nums) do |num|
+  num % 2 == 0 
+end
+  
