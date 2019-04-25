5bbed59f8fba575f945d20598bf24fd3f4245c98
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 20:51:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,10 @@
 def my_select(collection)
   i = 0 
-  new_arr = 
+  new_arr = []
   while i < collection.length
-    yield(collection)
+    new_arr << yield(collection)
   end
-  
+  new_arr
 end
 
 my_select(collection) do |num|
