2accd5c21f16188cd16eda488b87c6e84106d0c3
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 21:03:30 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
   i = 0 
   new_arr = []
   while i < collection.length
-    new_arr << yield(collection[i])
+    if yield(collection[i]) == true
+      new_arr = collection[i]
     i += 1 
   end
   new_arr
@@ -11,8 +12,6 @@ end
 nums = [1, 2, 3, 4, 5]
 
 my_select(nums) do |num|
-  if num % 2 == 0 
-    puts num
-  end
+  num % 2 == 0 
 end
   
